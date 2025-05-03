aa267f0ee4b33fb0660acac59cba2fbf49fe299a
Gabriel Arias <151084808+garias32@users.noreply.github.com>
2025-05-03 01:11:25 -0400

    Update terrain.py

--- terrain.py
+++ terrain.py
@@ -1,17 +1,16 @@
 from OpenGL.GL import *
 
-
 class Terrain:
-    def __init__(self):
-        # We'll keep it simple for now
-        pass
+    def __init__(self, size=50.0):
+        self.size = size
 
     def render_ground(self):
+        glColor3f(0.3,0.5,0.2)
         glBegin(GL_QUADS)
-        glColor3f(0.3, 0.5, 0.2)  # Grass color
-        glNormal3f(0, 1, 0)
-        glVertex3f(-50, 0, -50)
-        glVertex3f(-50, 0, 50)
-        glVertex3f(50, 0, 50)
-        glVertex3f(50, 0, -50)
+        glNormal3f(0,1,0)
+        s = self.size
+        glVertex3f(-s, 0, -s)
+        glVertex3f(-s, 0,  s)
+        glVertex3f( s, 0,  s)
+        glVertex3f( s, 0, -s)
         glEnd()
